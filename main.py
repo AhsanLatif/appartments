e68586c7c4ade299133435d7ea7b8ea089bd91b2
ahsanlatif <ahsanlatif18@gmail.com>
2019-10-07 00:55:01 +0500

    Flow completed in main.py. Code cleaned up

--- main.py
+++ main.py
@@ -3,32 +3,35 @@ from appartments.atira.locations import AppartmentScraper
 from appartments.atira.appartment_info import AppartmentInfoScraper
 
 url = "https://atira.com/locations/"
-url1 = "https://atira.com/location/merivale-st/"
-url2 = "https://atira.com/room/glen-rd/twin-share-3/"
 
-# apar = LocationScraper(url)
-# data_loc = apar.get_location_links()
-appartment_info = {}
-appar_info = AppartmentInfoScraper(url2)
-price = appar_info.get_price()
-capcacity = appar_info.capacity_of_persons()
-room_name = appar_info.room_name()
-building_name = appar_info.building_name()
-location = appar_info.location()
-feature = appar_info.features()
-appartment_info['price'] = price
-appartment_info['capacity'] = capcacity
-appartment_info['room_name'] = room_name
-appartment_info['building_name'] = building_name
-appartment_info['location'] = location
-appartment_info['features'] = feature
-print(appartment_info)
-#
-# apar = AppartmentScraper(url1)
-# app_loc = apar.get_appartment_links()
+apar = LocationScraper(url)
+location_links = apar.get_location_links()
+objs_appartment = []
+appartment_links = []
+for link in location_links:
+    objs_appartment.append(AppartmentScraper(link))
 
-# print(data_loc)
+for apprartment in objs_appartment:
+    appartment_links.append(apprartment.get_appartment_links())
 
-# data_appartments =
-# print(app_loc)
-print("Hello world!")
+appartments_info = []
+for links in appartment_links:
+    for link in links:
+        appartment_info = {}
+        appar_info = AppartmentInfoScraper(link)
+        print(link)
+        price = appar_info.get_price()
+        print(price)
+        capcacity = appar_info.capacity_of_persons()
+        room_name = appar_info.room_name()
+        building_name = appar_info.building_name()
+        location = appar_info.location()
+        feature = appar_info.features()
+        appartment_info['price'] = price
+        appartment_info['capacity'] = capcacity
+        appartment_info['room_name'] = room_name
+        appartment_info['building_name'] = building_name
+        appartment_info['location'] = location
+        appartment_info['features'] = feature
+        appartments_info.append(appartment_info)
+print(appartments_info)
