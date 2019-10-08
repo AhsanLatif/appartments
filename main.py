9fb9f7028271af6eb15e68785ad9e323993dc1a1
ahsanlatif <ahsanlatif18@gmail.com>
2019-10-08 08:12:07 +0500

    Added User Agents

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from appartments.atira.locations import LocationScraper
 from appartments.atira.appartments import AppartmentScraper
 from appartments.atira.appartment_info import AppartmentInfoScraper
-from appartments.constants import *
+from constants import *
 import json
+
 
 url = ATIRA_URL
 objs_appartment = []
@@ -29,4 +30,9 @@ for links in appartment_links:
         appartment_info['location'] = appar_info.location()
         appartment_info['features'] = appar_info.features()
         appartments_info.append(appartment_info)
+        print(appartments_info)
 print(json.dumps(appartments_info))
+
+# def average_prive_room(self, appartments):
+#     for appartment in appartments:
+
